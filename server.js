8d5c3935fc7c1d3938b9712cc607adfbb9e1fc14
Grzegorz Szewczyk <g.szewczyk196@gmail.com>
2022-05-26 22:36:46 +0200

    Heroku problem solution

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 const path = require("path")
-process.env.PORT || 3000; 
+var PORT = process.env.PORT || 3000; 
 
 app.use(express.json())
 app.use(express.static("static"))
